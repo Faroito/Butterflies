bbbb4baba6e5681c8b0fe4d1e4ba5225437fa5b3
Timothée Couble <Timothee@MacBook-Pro-de-Timothee-2.local>
2017-02-10 00:55:24 +0100

    Adding butterflies

--- script.js
+++ script.js
@@ -1,2 +1,73 @@
 var mainCanvas = document.getElementById("myCanvas");
 var mainContext = mainCanvas.getContext('2d');
+
+var butterflies = [];
+
+function setButterfly(size, xPos, yPos) {
+    for (var tmp = 0; tmp < 5000; tmp += 1)
+    {
+		var x = Math.sin(tmp) * (Math.exp(Math.cos(tmp)) - (2 * Math.cos(4 * tmp)) - Math.pow(Math.sin(tmp / 12), 6));
+		var y = Math.cos(tmp) * (Math.exp(Math.cos(tmp)) - (2 * Math.cos(4 * tmp)) - Math.pow(Math.sin(tmp / 12), 6));
+		x = -x * size + xPos;
+		y = -y * size + yPos;
+		mainContext.fillRect(x, y, 1, 1);
+    }
+}
+
+function Butterfly(size, xPos, yPos, color) {
+    this.size = size;
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.color = color;
+    this.opacity = 1;
+}
+
+Butterfly.prototype.update = function() {    
+    this.size += 0.2;
+    this.opacity -= 0.02;
+    mainContext.beginPath();
+    setButterfly(this.size, this.xPos, this.yPos);
+    mainContext.closePath();
+    mainContext.fillStyle = this.color.replace("[[opacity]]", this.opacity);
+    mainContext.fill();
+    return (this.opacity);
+};
+
+function randomColor() {
+	var red = Math.round(55 + Math.random() * 200);
+    var blue = Math.round(55 + Math.random() * 200);
+    var green = Math.round(55 + Math.random() * 200);
+	return ('rgba(' + red + ',' + blue + ',' + green + ", [[opacity]])");
+}
+
+function drawButterflies(number) {
+    for (var i = 0; i < number; i++) 
+    {
+    	var randomX = 100 + Math.round(Math.random() * 1300);
+        var randomY = 100 + Math.round(Math.random() * 600);
+        var size = 10 + Math.random() * 15;
+        var color = randomColor();
+        var butterfly = new Butterfly(size, randomX, randomY, color);
+        butterflies.push(butterfly);
+    }
+    draw();
+}
+
+function draw() {
+    mainContext.clearRect(0, 0, 1500, 800);
+    for (var i = 0; i < butterflies.length; i++) 
+    {
+        var myButterfly = butterflies[i];
+        limits = myButterfly.update();
+    }
+    if (limits > 0) {
+    	requestAnimationFrame(draw);
+    }
+}
+
+function sleep(ms) 
+{
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+drawButterflies(10);
