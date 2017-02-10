c5ee0ac7034183dddea843a640288f5d237db0b6
Timothée Couble <Timothee@MacBook-Pro-de-Timothee-2.local>
2017-02-10 13:24:31 +0100

    Adding changes

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var mainCanvas = document.getElementById("myCanvas");
 var mainContext = mainCanvas.getContext('2d');
 
+var generations = [];
 var butterflies = [];
 
-function setButterfly(size, xPos, yPos) {
-    for (var tmp = 0; tmp < 5000; tmp += 1)
+/*function setButterfly(size, xPos, yPos) {
+    for (var tmp = 0; tmp < 2000; tmp += 1)
     {
 		var x = Math.sin(tmp) * (Math.exp(Math.cos(tmp)) - (2 * Math.cos(4 * tmp)) - Math.pow(Math.sin(tmp / 12), 6));
 		var y = Math.cos(tmp) * (Math.exp(Math.cos(tmp)) - (2 * Math.cos(4 * tmp)) - Math.pow(Math.sin(tmp / 12), 6));
@@ -12,6 +13,10 @@ function setButterfly(size, xPos, yPos) {
 		y = -y * size + yPos;
 		mainContext.fillRect(x, y, 1, 1);
     }
+}*/
+
+function setButterfly(size, xPos, yPos) {
+	mainContext.fillRect(xPos, yPos, size, size);
 }
 
 function Butterfly(size, xPos, yPos, color) {
@@ -43,15 +48,29 @@ function randomColor() {
 function drawButterflies(number) {
     for (var i = 0; i < number; i++) 
     {
-    	var randomX = 100 + Math.round(Math.random() * 1300);
-        var randomY = 100 + Math.round(Math.random() * 600);
-        var size = 10 + Math.random() * 15;
+    	//var randomX = 100 + Math.round(Math.random() * 1300);
+        //var randomY = 100 + Math.round(Math.random() * 600);
+        var randomX = 10 + Math.round(Math.random() * 1460);
+        var randomY = 10 + Math.round(Math.random() * 760);
+        var size = 15 + Math.random() * 15;
         var color = randomColor();
         var butterfly = new Butterfly(size, randomX, randomY, color);
         butterflies.push(butterfly);
     }
     draw();
 }
+
+/*function draw() {
+    mainContext.clearRect(0, 0, 1500, 800);
+    for (var i = 0; i < butterflies.length; i++) 
+    {
+        var myButterfly = butterflies[i];
+        limits = myButterfly.update();
+    }
+    if (limits > 0) {
+    	requestAnimationFrame(draw);
+    }
+}*/
 
 function draw() {
     mainContext.clearRect(0, 0, 1500, 800);
@@ -63,11 +82,6 @@ function draw() {
     if (limits > 0) {
     	requestAnimationFrame(draw);
     }
-}
-
-function sleep(ms) 
-{
-  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 drawButterflies(10);
