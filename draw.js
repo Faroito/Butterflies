5d3a4f4fcc74cc8be2678b541509e5ae26de60f8
Timothée Couble <Timothee@MacBook-Pro-de-Timothee-2.local>
2017-02-15 23:32:33 +0100

    Get graph value

--- draw.js
+++ draw.js
@@ -1,20 +1,17 @@
 var mainCanvas = document.getElementById("my-canvas");
 var mainContext = mainCanvas.getContext('2d');
 var mainGraph = document.getElementById("my-graph");
-var widthGraph = document.getElementById("my-canvas").offsetHeight;
 var graphContext = mainGraph.getContext('2d');
-
-console.log(widthGraph);
 
 var k;
 var end;
 var i;
+var start;
 var nb;
 var speed;
 var myVar;
 
-var butterflies = [];
-var graph = [];
+/* Draw butterflies */
 
 function setButterfly(size, xPos, yPos) {
     mainContext.fillRect(xPos, yPos, size, size);
@@ -46,10 +43,14 @@ function drawButterflies(nb) {
     graphContext.fillStyle = color.replace("0.8", 1);;
 }
 
+/* Draw graph */
+
 function drawGraph(nb, i, end) {
     graphContext.fillRect((i * 300) / end, 300 - ((nb * 300) / 1000), 300 / end, 300);
     graphContext.fill();
 }
+
+/* Simulate all the generations */
 
 function generation() {
     drawButterflies(Math.round(nb));
@@ -62,9 +63,12 @@ function generation() {
     }
 }
 
+/* Get the input value */
+
 function getFormValues() {
     i = 0;
-    nb = document.getElementById("nbdepart").value;
+    start = document.getElementById("nbdepart").value;
+    nb = start;
     end = document.getElementById("nbgenerations").value;
     k = document.getElementById("growthrate").value;
     speed = document.getElementById("speed").value * 1000;
@@ -72,3 +76,28 @@ function getFormValues() {
     graphContext.clearRect(0, 0, 300, 300)
     generation();
 }
+
+/* For graph value with mouse */
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top};
+}
+
+mainGraph.addEventListener('mousemove', function(evt) {
+    var mousePos = getMousePos(mainGraph, evt);
+    for (i = 0; i < end ; i += 1) {
+        var min = (i * 300) / end + 10;
+        var max = min + 300 / end + 10;
+        if (min < mousePos.x && mousePos.x < max) {
+            var value = start;
+            for (j = 0; j < i; j += 1) {
+                value = k * value * ((1000 - value) / 1000);
+            }
+            console.log("Value = " + Math.round(value));
+        }
+    }
+}, false);
+
